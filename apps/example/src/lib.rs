47356cdbb7bc488ad4915ca9f52eb6badd46a249
Jason de Wolff <76750361+JasondeWolff@users.noreply.github.com>
2025-01-25 12:33:19 +0100

    🚀 Setup (#1)
    * setup
    
    * Update appearance.rs

---
+++ apps/example/src/lib.rs
@@ -0,0 +1,5 @@
+use appearance::Appearance;
+
+pub fn internal_main() {
+    let _appearance = Appearance::new("Example App");
+}
---
+++ crates/appearance/src/appearance.rs
@@ -0,0 +1,30 @@
+pub struct Appearance {}
+
+static APPEARANCE_STATIC: std::sync::OnceLock<AppearanceStatic> = std::sync::OnceLock::new();
+
+struct AppearanceStatic {
+    // TODO: Sentry telemetry
+}
+
+impl AppearanceStatic {
+    fn init(_app_name: &str) -> &'static Self {
+        appearance_profiling::profile_function!();
+
+        APPEARANCE_STATIC.get_or_init(|| {
+            env_logger::builder()
+                .filter_level(log::LevelFilter::Info)
+                .parse_default_env()
+                .init();
+
+            Self {}
+        })
+    }
+}
+
+impl Appearance {
+    pub fn new(app_name: &str) -> Self {
+        AppearanceStatic::init(app_name);
+
+        Self {}
+    }
+}
